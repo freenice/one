228efbcb8bf0a44f7c469dcd2854ccb31c69594b
zp3973 <1057220967@qq.com>
2016-04-09 10:29:47 +0800

    the one

---
+++ 99chengfa.py
@@ -0,0 +1,74 @@
+a = 0
+for i in range(1,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 1
+for i in range(2,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)   
+print '\n'
+a = 2
+for i in range(3,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+
+print '\n'
+a = 3
+for i in range(4,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 4
+for i in range(5,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 5
+for i in range(6,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 6
+for i in range(7,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 7
+for i in range(8,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 8
+for i in range(9,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+print '\n'
+a = 9
+for i in range(10,10):
+    while a <9:
+        a +=1
+        print '%d*%d=%d'%(i,a,i*a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
