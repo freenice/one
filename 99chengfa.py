8b43f4b8ee308c79136b38a95bff84b031af8c80
zp3973 <1057220967@qq.com>
2016-04-09 10:52:58 +0800

    the two

--- 99chengfa.py
+++ 99chengfa.py
@@ -1,4 +1,4 @@
-a = 0
+'''a = 0
 for i in range(1,10):
     while a <9:
         a +=1
@@ -57,7 +57,12 @@ a = 9
 for i in range(10,10):
     while a <9:
         a +=1
-        print '%d*%d=%d'%(i,a,i*a)
+        print '%d*%d=%d'%(i,a,i*a)'''
+
+for i in range(1,10):
+    for a in range(1,i+1):
+        print '%d*%d=%d\t'%(a,i,a*i),
+    print
 
 
 
